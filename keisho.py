977ce4c5f6e3ea6979befb2576bd688bb6b53ae3
namuro168 <n.amuro0312@gmail.com>
2024-09-01 19:26:10 +0900

    Create keisho.py

---
+++ keisho.py
@@ -0,0 +1,17 @@
+class TEST:
+    def __init__(self, txt, txt2):
+        self.txta = txt
+        self.txtb = txt2
+        print("__init__")
+    def add(self,majiA):
+        # wk1 = self.txta + txt
+
+        print("def add")
+        return self.txta,self.txtb
+
+if __name__ == '__main__':
+    obj = TEST(1, 5)
+    obj2 = TEST(2, 6)
+    print(obj)
+    p1 = obj.add(obj2)
+    print(p1)
